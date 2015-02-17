8b7dab2e3728879fd4a0eaecb9b5914bb4cff081
bartslinger <bartslinger@gmail.com>
2015-02-17 22:00:37 +0100

    [QtCreator] Removed hard-coded paths

--- sw/tools/qtcreator_ide_config/configure_workspace.py
+++ sw/tools/qtcreator_ide_config/configure_workspace.py
@@ -4,15 +4,24 @@ import sys
 import os
 import time
 
-# Set home directory
-PAPARAZZI_HOME = os.path.realpath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '../../../')
+# Set home directory and unittest directory
+TOOL_DIR       = os.path.dirname(os.path.realpath(__file__))
+PAPARAZZI_HOME = os.path.realpath(os.path.join(TOOL_DIR, os.pardir, os.pardir, os.pardir))
+TEST_HOME      = os.path.join(PAPARAZZI_HOME, "tests", "unittest")
 
 # Aircraft name is argument and should be equal to project name
 AIRCRAFT = sys.argv[1]
 
 # Check if the IDE files exist with that name
-if not os.path.isfile(PAPARAZZI_HOME + "/" + AIRCRAFT + ".creator"):
+if not os.path.isfile(os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".creator")):
 	raise Exception("Aircraft name does not match project name.")
+
+# Check if also a test environment is configured for this aircraft
+if not os.path.isfile(os.path.join(TEST_HOME, AIRCRAFT + "Tester.creator")):
+	print "No test environment configured for this aircraft. To do so, create a project called \"" + AIRCRAFT + "Tester\" in " + TEST_HOME + "."
+	TEST_CONF = False
+else:
+	TEST_CONF = True
 
 # First, do make clean on the aircraft
 cmd = "make -C " + PAPARAZZI_HOME + " -f Makefile.ac AIRCRAFT=" + AIRCRAFT + " clean_ac"
@@ -23,20 +32,21 @@ reply = os.popen(cmd).read()
 print "========================"
 
 # Remove project files
-# If the interpeter runs for the first time, it regenerates these files. Only the first time
-config_file   = PAPARAZZI_HOME + "/" + AIRCRAFT + ".config"
-includes_file = PAPARAZZI_HOME + "/" + AIRCRAFT + ".includes"
-files_file    = PAPARAZZI_HOME + "/" + AIRCRAFT + ".files"
-test_config_file   = PAPARAZZI_HOME + "/tests/unittest/" + AIRCRAFT + "Tester.config"
-test_includes_file = PAPARAZZI_HOME + "/tests/unittest/" + AIRCRAFT + "Tester.includes"
+# If the gcc_interpeter.py runs for the first time, it regenerates these files. Only the first time
+config_file   = os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".config")
+includes_file = os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".includes")
+files_file    = os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".files")
 if os.path.isfile(config_file):
 	os.remove(config_file)
 if os.path.isfile(includes_file):
 	os.remove(includes_file)
-if os.path.isfile(test_config_file):
-	os.remove(test_config_file)
-if os.path.isfile(test_includes_file):
-	os.remove(test_includes_file)
+if TEST_CONF:
+	test_config_file   = os.path.join(TEST_HOME, AIRCRAFT + "Tester.config")
+	test_includes_file = os.path.join(TEST_HOME, AIRCRAFT + "Tester.includes")
+	if os.path.isfile(test_config_file):
+		os.remove(test_config_file)
+	if os.path.isfile(test_includes_file):
+		os.remove(test_includes_file)
 
 # create empty files file
 if os.path.isfile(files_file):
@@ -46,35 +56,46 @@ new_files_file.close()
 
 # Then, do a full build on the aircraft, but replace compiler with python script
 print "Analyzing make output.."
-cmd = "make -f Makefile.ac PAPARAZZI_HOME=" + PAPARAZZI_HOME + " PAPARAZZI_SRC=" + PAPARAZZI_HOME + " AIRCRAFT=" + AIRCRAFT + " CC='python " + PAPARAZZI_HOME + "/sw/tools/qtcreator_ide_config/gcc_interpeter.py " + AIRCRAFT + "' CP='python " + PAPARAZZI_HOME + "/sw/tools/qtcreator_ide_config/skip_command.py' LD='python " + PAPARAZZI_HOME + "/sw/tools/qtcreator_ide_config/skip_command.py' DMP='python " + PAPARAZZI_HOME + "/sw/tools/qtcreator_ide_config/skip_command.py' NM='python " + PAPARAZZI_HOME + "/sw/tools/qtcreator_ide_config/skip_command.py' NPROCS=1 ap.compile"
-print os.popen(cmd).read()
+cmd = "make -f Makefile.ac PAPARAZZI_HOME=" + PAPARAZZI_HOME + " PAPARAZZI_SRC=" + PAPARAZZI_HOME + " AIRCRAFT=" + AIRCRAFT + " CC='python " + os.path.join(TOOL_DIR, "gcc_interpeter.py") + " " + AIRCRAFT + "' CP='python " + os.path.join(TOOL_DIR, "skip_command.py") + "' LD='python " + os.path.join(TOOL_DIR, "skip_command.py") + "' DMP='python " + os.path.join(TOOL_DIR, "skip_command.py") + "' NM='python " + os.path.join(TOOL_DIR, "skip_command.py") + "' NPROCS=1 ap.compile"
+output = os.popen(cmd).read()
+print "Done analyzing make output."
 
-# Get all the defines from config file
-UNITTEST_HOME = PAPARAZZI_HOME + "/tests/unittest"
-test_config_file = open(PAPARAZZI_HOME + "/tests/unittest/" + AIRCRAFT + "Tester.config")
-macro_text = test_config_file.read()
-test_config_file.close()
 
-macro_flags = macro_text.split('\n')
-for i, s in enumerate(macro_flags):
-	macro_flags[i] = s.replace("#define ", "    - -D").replace(" ", "='").replace("='='='='-='-D", "    - -D") + "'" #beun
+## The next section generates the yml file used for unittesting.
+if TEST_CONF:
+	print "========================"
+	print "Configuring unittest framework.."
+	# Get all the defines from config file.
 
-macro_flags.pop()
+	test_config_file = open(os.path.join(TEST_HOME, AIRCRAFT + "Tester.config"))
+	macro_text = test_config_file.read()
+	test_config_file.close()
+	macro_flags = macro_text.split("\n")
+	for i, s in enumerate(macro_flags):
+		# defines in <AIRCRAFT>.yml
+		macro_flags[i] = "    - -D" + s.replace("#define ", "").replace(" ", "='") + "'"
 
-# Get include directories from testing includes file
-test_includes_file = open(UNITTEST_HOME + "/" + AIRCRAFT + "Tester.includes")
-test_includes_content = test_includes_file.read().split('\n')
-for i, s in enumerate(test_includes_content):
-	test_includes_content[i] = "      - '" + s + "/'"
-yml_include_items = '\n'.join(test_includes_content)
+	macro_flags.pop()
+	yml_macro_flags = "\n".join(macro_flags)
 
-# Configure the yml script for unit testing
-original_yml = open(UNITTEST_HOME + "/ymlmodel")
-original_content = original_yml.read()
-original_yml.close()
+	# Get include directories from testing includes file
+	test_includes_file = open(os.path.join(TEST_HOME, AIRCRAFT + "Tester.includes"))
+	test_includes_content = test_includes_file.read().split("\n")
+	test_includes_file.close()
+	for i, s in enumerate(test_includes_content):
+		test_includes_content[i] = "      - '" + s + "/'"
+	
+	test_includes_content.pop()
+	yml_include_items = "\n".join(test_includes_content)
 
-yml_macro_flags = '\n'.join(macro_flags)
-new_content = original_content.replace("MACRO_ITEMS", yml_macro_flags).replace("INCLUDE_ITEMS", yml_include_items)
-new_yml = open(UNITTEST_HOME + "/" + AIRCRAFT + ".yml", "w")
-new_yml.write(new_content)
-new_yml.close()
+	# Configure the yml script for unit testing
+	original_yml = open(os.path.join(TEST_HOME, "template.yml"))
+	original_content = original_yml.read()
+	original_yml.close()
+
+	new_content = original_content.replace("MACRO_ITEMS", yml_macro_flags).replace("INCLUDE_ITEMS", yml_include_items)
+	new_yml = open(os.path.join(TEST_HOME, AIRCRAFT + ".yml"), "w")
+	new_yml.write(new_content)
+	new_yml.close()
+	print "Done completing unittest framework."
+	print "========================"
--- sw/tools/qtcreator_ide_config/gcc_interpeter.py
+++ sw/tools/qtcreator_ide_config/gcc_interpeter.py
@@ -4,85 +4,101 @@ import sys
 import os
 import re
 
-# Set home directory
-PAPARAZZI_HOME = os.path.realpath(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '../../../')
+# Set home directory and unittest directory
+TOOL_DIR       = os.path.dirname(os.path.realpath(__file__))
+PAPARAZZI_HOME = os.path.realpath(os.path.join(TOOL_DIR, os.pardir, os.pardir, os.pardir))
+TEST_HOME      = os.path.join(PAPARAZZI_HOME, "tests", "unittest")
 
-# Aircraft name substituted in arguments
-#if not sys.argv[2] == "-MMD" :
-
-# Only look at gcc command
+# Only look at gcc command in which compilation of an object takes place. This has the -MMD flag.
 if not (sys.argv[2] == "-MMD"):
 	quit()
 
+# Aircraft name substituted in arguments by configure_workspace.py
 AIRCRAFT = sys.argv[1]
 
+# Check if also a test environment is configured for this aircraft
+if not os.path.isfile(os.path.join(TEST_HOME, AIRCRAFT + "Tester.creator")):
+	TEST_CONF = False
+else:
+	TEST_CONF = True
+
 # Update config+includes only once (this script is called many times, but config and includes are always the same)
-if not os.path.isfile(PAPARAZZI_HOME + "/" + AIRCRAFT + ".config"):
+if not os.path.isfile(os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".config")):
 	print "Generating config and includes file"
-	config   = open(PAPARAZZI_HOME + "/" + AIRCRAFT + ".config", "w")
-	includes = open(PAPARAZZI_HOME + "/" + AIRCRAFT + ".includes", "w")
-	test_config   = open(PAPARAZZI_HOME + "/tests/unittest/" + AIRCRAFT + "Tester.config", "w")
-	test_includes = open(PAPARAZZI_HOME + "/tests/unittest/" + AIRCRAFT + "Tester.includes", "w")
+	config   = open(os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".config"), "w")
+	includes = open(os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".includes"), "w")
+	if TEST_CONF:
+		test_config   = open(os.path.join(TEST_HOME, AIRCRAFT + "Tester.config"), "w")
+		test_includes = open(os.path.join(TEST_HOME, AIRCRAFT + "Tester.includes"), "w")
 
 	for argument in sys.argv:
+		# Look for flag -Ddefine=value
 		matchObj = re.match(r'-D(.+)=(.*)', argument)
 		if(matchObj) :
-			newDefine = '#define ' + matchObj.group(1) + ' ' + matchObj.group(2) + '\n'
+			newDefine = "#define " + matchObj.group(1) + " " + matchObj.group(2) + "\n"
 			newDefine = newDefine.replace('\\"', '"')
 			newDefine = newDefine.replace('\'', '')
 			config.write(newDefine)
-			test_config.write(newDefine)
+			if TEST_CONF:
+				test_config.write(newDefine)
 			continue
 
+		# Look for flag -Ddefine (without value)
 		matchObj = re.match(r'-D(.+)', argument)
 		if(matchObj) :
 			newDefine = '#define ' + matchObj.group(1) + ' 1\n'
 			config.write(newDefine)
-			test_config.write(newDefine)
+			if TEST_CONF:
+				test_config.write(newDefine)
 			continue
 
+		# Look for flag include path
 		matchObj = re.match(r'-I(.+)', argument)
 		if(matchObj) :
-			newInclude = os.path.normpath(os.path.join(PAPARAZZI_HOME, './sw/airborne', matchObj.group(1)))
-			includes.write(newInclude + '\n')
-			test_includes.write(newInclude + '\n')
-			newInclude = os.path.normpath(os.path.join(PAPARAZZI_HOME + "/tests/unittest", './sw/airborne', matchObj.group(1)))
-			test_includes.write(newInclude + '\n')
-			#incc.write('-I' + newInclude + ' ')
+			newInclude = os.path.normpath(os.path.join(PAPARAZZI_HOME, "sw", "airborne", matchObj.group(1)))
+			includes.write(newInclude + "\n")
+			if TEST_CONF:
+				test_includes.write(newInclude + "\n")
+				newInclude = os.path.normpath(os.path.join(TEST_HOME, "sw", "airborne", matchObj.group(1)))
+				test_includes.write(newInclude + "\n")
+	# end for loop
 
-	# extra includes for unittest
-	test_includes.write('/home/bart/unity/src\n')
-	test_includes.write('/home/bart/unity/extras/fixture/src\n')
 	# close the project files
 	config.close()
 	includes.close()
-	test_config.close()
-	test_includes.close()
+	if TEST_CONF:
+		# extra includes for unittest
+		test_includes.write(os.path.join(TEST_HOME, "cmock", "src") + "\n")
+		test_includes.write(os.path.join(TEST_HOME, "unity", "src") + "\n")
+		test_includes.write(os.path.join(TEST_HOME, "unity", "extras", "fixture", "src") + "\n")
+		# close test project files
+		test_config.close()
+		test_includes.close()
 
 # The files-file must be update every every call, to resolve all dependencies
 # First, extract all files currently in there
-files_file = open(PAPARAZZI_HOME + "/" + AIRCRAFT + ".files", "r")
-all_files = files_file.read().split('\n')
-all_files.remove('')
+files_file = open(os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".files"), "r")
+all_files = files_file.read().split("\n")
+all_files.remove("")
 discovered_files = []
 
 # Generate includeflags from projects includes file
-includes = open(PAPARAZZI_HOME + "/" + AIRCRAFT + ".includes")
-includescontent = includes.read().split('\n')
-includescontent.remove('')
-includeflags = ''
+includes = open(os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".includes"))
+includescontent = includes.read().split("\n")
+includescontent.remove("")
+includeflags = ""
 for incdir in includescontent:
-	includeflags += '-I' + incdir + ' '
+	includeflags += "-I" + incdir + " "
 includes.close()
 
 # Extract c file from arguments
 for argument in sys.argv:
 	if(argument.endswith('.c')):
-		c_file = PAPARAZZI_HOME + "/sw/airborne/" + argument
+		c_file = os.path.join(PAPARAZZI_HOME, "sw", "airborne", argument)
 		discovered_files.append(c_file)
 		
 		# For this c-file, resolve all dependencies
-		cmd = "gcc -MM " + c_file + " " + includeflags + " -include" + PAPARAZZI_HOME + "/" + AIRCRAFT + ".config"
+		cmd = "gcc -MM " + c_file + " " + includeflags + " -include" + os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".config")
 		headers = os.popen(cmd).read().split('\\')
 		# Start at 2 because 0=.c and 1=.config
 		for i in range(2, len(headers)-1):
@@ -96,8 +112,8 @@ for newfile in discovered_files:
 
 # Rewrite the files-file
 files_file.close()
-files_file = open(PAPARAZZI_HOME + "/" + AIRCRAFT + ".files", "w")
+files_file = open(os.path.join(PAPARAZZI_HOME, AIRCRAFT + ".files"), "w")
 for xfile in all_files:
-	files_file.write(xfile + '\n')
+	files_file.write(xfile + "\n")
 
 files_file.close()
