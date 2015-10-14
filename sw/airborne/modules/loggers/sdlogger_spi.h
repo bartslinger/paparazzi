9fa9c763a640eaae4e306c2604d14ddba78dd494
Bart Slinger <bartslinger@gmail.com>
2015-10-14 17:22:45 +0200

    SD Logger module created

---
+++ sw/airborne/modules/loggers/sdlogger_spi.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) Bart Slinger
+ *
+ * This file is part of paparazzi
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file "modules/loggers/sdlogger_spi.h"
+ * @author Bart Slinger
+ * SPI SD Logger that saves pprzlog messages to SD Card.
+ */
+
+#ifndef SDLOGGER_SPI_H
+#define SDLOGGER_SPI_H
+
+// extern void sdlogger_spi_init();
+// extern void sdlogger_spi_periodic();
+
+#endif
+
